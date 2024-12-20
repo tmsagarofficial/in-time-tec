3feaae0786020949fb5f0b1a5e8629ea70cff011
S A G A R <110724849+tmsagarofficial@users.noreply.github.com>
2024-12-20 22:38:24 +0530

    Updated Logic of string validity and improved functions with edge cases

--- calculator.c
+++ calculator.c
@@ -2,48 +2,70 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#include<stdbool.h>
 #define MAX_EXPRESSION_LENGTH 100
 
-// Validating the expression input about its syntactical corretcness.
-bool checkValidityOfString(const char* expr) {
+
+int isitdigit(char c);
+int checkValidityOfString(const char *expr);
+int calculator(char *expr);
+
+int isitdigit(char c){
+    return c >= '0' && c <= '9';
+}
+
+int checkValidityOfString(const char* expr) {
     int lengthOfExpression = strlen(expr); 
-    int digitsCount= 0;
+    int digitsCount = 0;
     int operatorsCount = 0;
-    bool wasOperator=true;
+    int wasOperator = 1;
+    int consecutiveNegatives = 0;
 
     for (int i = 0; i < lengthOfExpression; i++) {
-        // This loop is to count the number of digits and operators and update the same in variables digitsCount and operatorsCount.
-        if (expr[i] == ' '){
+        if (expr[i] == ' ') {
+            // Check if space is improperly placed between digits.
+            if (i > 0 && isitdigit(expr[i - 1]) && isitdigit(expr[i + 1])) {
+                return 0; // Invalid if space is splitting digits.
+            }
             continue;
-        } 
-
-        if (isdigit(expr[i])) {
+        }
+        if (isitdigit(expr[i])) {
             digitsCount++;
-            wasOperator = false;
+            while (i + 1 < lengthOfExpression && isitdigit(expr[i + 1])) {
+                i++;
+                digitsCount++;
+            }
+            wasOperator = 0;
+            consecutiveNegatives = 0;
         } 
         else if (strchr("+-*/", expr[i])) {
-            
-            if (wasOperator && expr[i] != '-') {// To consider negative numbers.
-                return false; 
+            if (expr[i] == '-') {
+                if (wasOperator) {
+                    consecutiveNegatives++;
+                    continue;
+                }
+            } else if (wasOperator) {
+                return 0;
             }
             operatorsCount++;
-            wasOperator = true;
+            wasOperator = 1;
         } else {
-            return false; // Invalid characters found in the expression.
+            return 0; 
         }
     }
 
-    return (digitsCount> operatorsCount); // Valid if only all the operators are between digits i.e we should hav more number of digits then operators. 
+    return (digitsCount > operatorsCount); // Valid if more digits than operators.
 }
-// This is to address the precedence of the operators i.e DMAS as per the requirements.
+
+
 int precedence(char op) {
     if (op == '*' || op == '/') return 2;
     if (op == '+' || op == '-') return 1;
     return 0;
 }
 
-// Performing the calculation for the given operands and a given operator.
+
+
+
 int calculate(int a, int b, char op) {
     switch (op) {
         case '+': return a + b;
@@ -52,41 +74,46 @@ int calculate(int a, int b, char op) {
         case '/': 
             if (b == 0) {
                 printf("Error: Division by zero.\n");
-                exit(1);
+                return 1;
             }
             return a / b;
         default: return 0;
     }
 }
 
-// Evaluating  the mathematical expression.
+
 int evaluateExpression(char* expr) {
-    int digitsStack[MAX_EXPRESSION_LENGTH]; // Stack for digits.
-    char operatorsStack[MAX_EXPRESSION_LENGTH];  // Stack for operators.
+    int digitsStack[MAX_EXPRESSION_LENGTH]; 
+    char operatorsStack[MAX_EXPRESSION_LENGTH]; 
     int topOfDigitsStack = -1, topOfOperatorsStack = -1;
+    int consecutiveNegatives = 0;
 
     for (int i = 0; expr[i] != '\0'; i++) {
         if (expr[i] == ' '){
             continue;
         }
-
-        if (isdigit(expr[i])) {
+        if (isitdigit(expr[i])) {
             int num = 0;
-            while (isdigit(expr[i])) {
-                num = num * 10 + (int)(expr[i] - '0'); // Getting the digitsvalue from the string expression.
+            while (isitdigit(expr[i])) {
+                num = num * 10 + (int)(expr[i] - '0'); 
                 i++;
             }
             i--;
-            digitsStack[++topOfDigitsStack] = num; // Pushing dgits to digits stack.
+            if(consecutiveNegatives % 2 == 1){
+                num = -num;
+            }
+            consecutiveNegatives = 0;
+            digitsStack[++topOfDigitsStack] = num; 
         } else if (strchr("+-*/", expr[i])) {
             // Processing operators based on precedence.
-            while (topOfOperatorsStack >= 0 && precedence(operatorsStack[topOfOperatorsStack]) > precedence(expr[i])) {
+            while (topOfOperatorsStack >= 0 && precedence(operatorsStack[topOfOperatorsStack]) >= precedence(expr[i])){
                 int num1 = digitsStack[topOfDigitsStack--];
                 int num2 = digitsStack[topOfDigitsStack--];
-                char operator = operatorsStack[topOfOperatorsStack--];
-                digitsStack[++topOfDigitsStack] = calculate(num2, num1, operator);
+                char op = operatorsStack[topOfOperatorsStack--];
+                digitsStack[++topOfDigitsStack] = calculate(num2, num1, op);
             }
-            operatorsStack[++topOfOperatorsStack] = expr[i]; // Pushing current operator to operators stack.
+            operatorsStack[++topOfOperatorsStack] = expr[i];
+            consecutiveNegatives = 0;
         }
     }
 
@@ -94,27 +121,24 @@ int evaluateExpression(char* expr) {
     while (topOfOperatorsStack >= 0) {
         int num1 = digitsStack[topOfDigitsStack--];
         int num2 = digitsStack[topOfDigitsStack--];
-        char operator = operatorsStack[topOfOperatorsStack--];
-        digitsStack[++topOfDigitsStack] = calculate(num2, num1, operator);
+        char op = operatorsStack[topOfOperatorsStack--];
+        digitsStack[++topOfDigitsStack] = calculate(num2, num1, op);
     }
 
     return digitsStack[topOfDigitsStack];
 }
 
 int main() {
-    char expr[MAX_EXPRESSION_LENGTH];// To store the mathematical expression of the string data-type.
-
+    char expr[MAX_EXPRESSION_LENGTH];
     printf("Enter the mathematical expression to evaulate: ");
     fgets(expr, sizeof(expr), stdin);
-    expr[strcspn(expr, "\n")] = 0; // Removing the newline character which would come when we press enter key.
+    expr[strcspn(expr, "\n")] = 0; 
 
     if (!checkValidityOfString(expr)) {
         printf("Error: Invalid expression.\n");
-        return 1;
+        goto end;
     }
-
-    
     printf("\nResult: %d\n", evaluateExpression(expr));
-
-    return 0;
+    end:
+        return 0;
 }
