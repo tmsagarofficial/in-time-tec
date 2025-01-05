a280b1cfa814d5339c07c33a434892be822cd102
S A G A R <110724849+tmsagarofficial@users.noreply.github.com>
2025-01-05 23:22:47 +0530

    practice

---
+++ week-2/05-01-2025/digitFrequency.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    char s[1000];
+    int freq[10] = {0}; 
+
+    scanf("%[^\n]", s); 
+    for (int i = 0; i < strlen(s); i++) {
+        if (s[i] >= '0' && s[i] <= '9') {
+            freq[s[i] - '0']++;
+        }
+    }
+
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", freq[i]);
+    }  
+    return 0;
+}
